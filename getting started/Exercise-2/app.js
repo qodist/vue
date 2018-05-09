320fedf3d456272d833b8ccecd37b3525d10fbaa
qodist <reyno75@gmail.com>
2018-05-09 20:03:20 +0300

    redoing lessons

--- getting started/Exercise-2/app.js
+++ getting started/Exercise-2/app.js
@@ -2,5 +2,13 @@ new Vue({
         el: '#exercise',
         data: {
             value: ''
+        },
+        methods: {
+            alertMe: function(){
+                alert("You rang........");
+            },
+            userInput: function(event){
+                this.value = event.target.value;
+            }
         }
     });
